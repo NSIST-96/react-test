eb725e4b64b175d5043b832e0426ddac82d6772e
NSIST96 <amelin.good@mail.ru>
2023-01-08 23:55:09 +0300

    Refactoring

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,7 +2,12 @@ import React from "react"
 
 const Content = () =>{
     return <div className='content'>
-        Content
+        <img src="https://abc-decor.com/img/gallery/12/thumbs/thumb_l_PL35736.jpg" alt="img"></img>
+        <p>Ava + name</p>
+        <p>Info</p>
+        <p>New post</p>
+        <p>Post 1</p>
+        <p>Post 2</p>
     </div>
 }
 
