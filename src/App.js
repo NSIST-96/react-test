1e399531ddb8e7ae605e862a94b5baf08e5b4b12
NSIST96 <amelin.good@mail.ru>
2023-01-15 22:19:06 +0300

    Combine jsx & css components in its own dir-s

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Navbar from './Components/Navbar';
+import Content from './Components/Content/Content.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+import Header from './Components/Header/Header.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
 
 const App = () => {
   return (
