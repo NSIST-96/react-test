8e4bb495b8dd1a05c2e2aa3f4567c63846852d06
NSIST96 <amelin.good@mail.ru>
2023-01-17 16:41:56 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Profile from './Components/Profile/Profile.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import Header from './Components/Header/Header.jsx';
 import Navbar from './Components/Navbar/Navbar.jsx';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      <Dialogs />
       <Footer />
     </div>
   );
